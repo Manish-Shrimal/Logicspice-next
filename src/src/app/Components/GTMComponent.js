06fabb097d3b86b45fd537e28cdcbdbef72dcc2d
Manish-shrimal <manish@logicspice.com>
2024-10-28 12:23:56 +0530

    Added changes for performance

---
+++ src/src/app/Components/GTMComponent.js
@@ -0,0 +1,85 @@
+"use client"
+// import { useEffect } from 'react';
+// import Script from 'next/script';
+
+// export default function GTMComponent() {
+//   useEffect(() => {
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag() {
+//       dataLayer.push(arguments);
+//     }
+//     gtag('js', new Date());
+//     gtag('config', 'AW-946594877');
+//     gtag('config', 'G-080ZLKT6JT');
+//     gtag('config', 'G-ZXWWBTSP0T');
+//   }, []);
+
+//   return (
+//     <>
+//       {/* Google Tag Manager */}
+//       <Script
+//         id="google-tag-manager"
+//         src="https://www.googletagmanager.com/gtag/js?id=AW-946594877"
+//         strategy="afterInteractive"
+//       />
+      
+//       <Script id="google-tag-manager-setup" strategy="afterInteractive">
+//         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//           })(window,document,'script','dataLayer','GTM-5357VDW');`}
+//       </Script>
+
+//       {/* Global site tag (gtag.js) - Google Analytics */}
+//       <Script
+//         id="google-analytics"
+//         src="https://www.googletagmanager.com/gtag/js?id=UA-19456135-5"
+//         strategy="afterInteractive"
+//       />
+//     </>
+//   );
+// }
+
+
+
+import { useEffect } from 'react';
+import Script from 'next/script';
+
+export default function GTMComponent() {
+  useEffect(() => {
+    // Initialize dataLayer if not already done
+    window.dataLayer = window.dataLayer || [];
+    // Function to push data to dataLayer
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    // Initialize Google Analytics configurations
+    gtag('js', new Date());
+    gtag('config', 'AW-946594877');
+    gtag('config', 'G-080ZLKT6JT');
+    gtag('config', 'G-ZXWWBTSP0T');
+  }, []);
+
+  return (
+    <>
+      {/* Google Tag Manager - Lazy load */}
+      <Script
+        id="google-tag-manager"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-946594877"
+        strategy="lazyOnload"
+      />
+      
+      <Script id="google-tag-manager-setup" strategy="lazyOnload">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.defer=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5357VDW');`}
+      </Script>
+
+      {/* Google Analytics - Lazy load */}
+      <Script
+        id="google-analytics"
+        src="https://www.googletagmanager.com/gtag/js?id=UA-19456135-5"
+        strategy="lazyOnload"
+      />
+    </>
+  );
+}
