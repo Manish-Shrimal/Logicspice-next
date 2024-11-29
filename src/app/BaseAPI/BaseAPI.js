db2d63e74bd8ccd4e95b9d3481e31ebd02bc8626
Manish-shrimal <manish@logicspice.com>
2024-11-29 12:08:35 +0530

    Uploading again with demo backend path

--- src/app/BaseAPI/BaseAPI.js
+++ src/app/BaseAPI/BaseAPI.js
@@ -1,3 +1,4 @@
-const BaseAPI = "https://www.manage.logicspice.com/api";
+// const BaseAPI = "https://www.manage.logicspice.com/api";
+const BaseAPI = "https://lswebsitedemo.logicspice.com/logicspice/api";
 
 export default BaseAPI;
