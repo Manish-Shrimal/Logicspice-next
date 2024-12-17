aef3e7a6cabecd968dd896db60a63ecb3fd50dba
Manish-shrimal <manish@logicspice.com>
2024-12-17 11:45:35 +0530

    changed the base API

--- src/app/BaseAPI/BaseAPI.js
+++ src/app/BaseAPI/BaseAPI.js
@@ -1,4 +1,4 @@
-// const BaseAPI = "https://www.manage.logicspice.com/api";
-const BaseAPI = "https://lswebsitedemo.logicspice.com/logicspice/api";
+const BaseAPI = "https://manage.logicspice.com/api";
+// const BaseAPI = "https://lswebsitedemo.logicspice.com/logicspice/api";
 
 export default BaseAPI;
