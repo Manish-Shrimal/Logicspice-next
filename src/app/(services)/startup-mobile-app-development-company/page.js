a439b5fc83aaea21b0c685ec232958008200f590
Manish-shrimal <manish@logicspice.com>
2025-01-09 18:03:55 +0530

    added new changes

--- src/app/(services)/startup-mobile-app-development-company/page.js
+++ src/app/(services)/startup-mobile-app-development-company/page.js
@@ -85,14 +85,14 @@ const Page = () => {
           </div>
         </div>
       </section>
-      {/* <section className="innovative_startup">
+      <section className="innovative_startup">
         <div className="container">
           <h2 className="title_main">
-            Our Latest Additions: Innovative Services and Software Solutions for
+            Innovative Services and Software Solutions for
             Startups
           </h2>
           <h4>Helping Startups Succeed with Smart Solutions</h4>
-          <p>
+          {/* <p>
             At Logicspice, we know how important it is for startups to have the
             right tools and technology to grow. That&apos;s why we&apos;ve added services
             and software solutions that make a real difference.
@@ -133,10 +133,46 @@ const Page = () => {
             Let us help your startup turn your ideas into success. Whether
             you&apos;re just starting out or looking to grow, we&apos;ve got the right
             tools and support to help you move forward.
+          </p> */}
+          <p>
+            At Logicspice, we know how important it is for startups to have the
+            right tools and technology to grow. That&apos;s why we&apos;ve added
+            services and software solutions that make a real difference. Our{" "}
+            <Link href="/blockchain-development">
+              {" "}
+              Blockchain Development Services{" "}
+            </Link>{" "}
+            helps startups create secure platforms for things like smart
+            contracts and decentralized apps, giving your business a strong
+            foundation of trust.
           </p>
           <p>
-            Ready to get started? Reach out to us today and let&apos;s chat about how
-            our services can help your startup thrive!
+            In addition, our Generative AI Development services empower startups
+            to automate tasks, enhance creativity, and boost productivity with
+            advanced AI models. To help you tell your brand&apos;s story, our Video
+            Creation Services are designed to create compelling visuals—perfect
+            for pitching to investors or attracting customers.
+          </p>
+          <p>
+            We also offer ready-to-use software to make running your business
+            easier. From Doctor Appointment Booking Software for healthcare
+            startups to Salon Booking Software for beauty and wellness
+            businesses, we&apos;ve got you covered. Our Group Chat Apps are perfect
+            for improving internal communication and team collaboration, while
+            our Live Chat Software helps you engage with customers in real time,
+            boosting satisfaction and conversions. We also provide{" "}
+            <Link href="/booking-software-solutions">
+              customizable booking solutions
+            </Link>{" "}
+            for various service-based businesses, ensuring smooth and easy
+            interactions for your clients. Let us help your startup turn your
+            ideas into success. Whether you&apos;re just starting out or looking to
+            grow, we&apos;ve got the right tools and support to help you move
+            forward.
+          </p>
+          <p>
+            Ready to get started? Reach out to us today and let&apos;s chat
+            about how our services can help your startup thrive!
           </p>
           <div className="mt-5">
             <Link href="/contact-us" className="btn btn-startup">
@@ -144,14 +180,14 @@ const Page = () => {
             </Link>
           </div>
         </div>
-      </section> */}
+      </section>
 
       <section className="working_process">
         <div className="container">
           <h2 className="title_main">How We Nourish Your Startup Business?</h2>
           <div className="work_section">
             <div className="row">
-              <div className="col-sm-4 col-md-6" >
+              <div className="col-sm-4 col-md-6">
                 <div className="image_working right_img">
                   <Image
                     unoptimized={true}
@@ -162,7 +198,7 @@ const Page = () => {
                   />
                 </div>
               </div>
-              <div className="col-sm-8 col-md-6" >
+              <div className="col-sm-8 col-md-6">
                 <div className="idea_working">
                   <h3>Innovation as a Core Value</h3>
                   <p>
@@ -176,7 +212,7 @@ const Page = () => {
             </div>
           </div>
           <div className="work_section">
-            <div className="col-sm-4 col-md-6 pull-right" >
+            <div className="col-sm-4 col-md-6 pull-right">
               <div className="image_working right_img">
                 <Image
                   unoptimized={true}
@@ -187,7 +223,7 @@ const Page = () => {
                 />
               </div>
             </div>
-            <div className="col-sm-8 col-md-6 pull-left" >
+            <div className="col-sm-8 col-md-6 pull-left">
               <div className="idea_working">
                 <h3>Stand Ahead From Competition</h3>
                 <p>
@@ -201,7 +237,7 @@ const Page = () => {
           </div>
           <div className="work_section">
             <div className="row">
-              <div className="col-sm-4 col-md-6" >
+              <div className="col-sm-4 col-md-6">
                 <div className="image_working right_img">
                   <Image
                     unoptimized={true}
@@ -212,7 +248,7 @@ const Page = () => {
                   />
                 </div>
               </div>
-              <div className="col-sm-8 col-md-6" >
+              <div className="col-sm-8 col-md-6">
                 <div className="idea_working">
                   <h3>Skilled Developers</h3>
                   <p>
@@ -229,7 +265,7 @@ const Page = () => {
             </div>
           </div>
           <div className="work_section">
-            <div className="col-sm-4 col-md-6 pull-right" >
+            <div className="col-sm-4 col-md-6 pull-right">
               <div className="image_working">
                 <Image
                   unoptimized={true}
@@ -240,7 +276,7 @@ const Page = () => {
                 />
               </div>
             </div>
-            <div className="col-sm-8 col-md-6 pull-left" >
+            <div className="col-sm-8 col-md-6 pull-left">
               <div className="idea_working">
                 <h3>Interactive UI</h3>
                 <p>
@@ -254,7 +290,7 @@ const Page = () => {
           </div>
           <div className="work_section">
             <div className="row">
-              <div className="col-sm-4 col-md-6" >
+              <div className="col-sm-4 col-md-6">
                 <div className="image_working">
                   <Image
                     unoptimized={true}
@@ -265,7 +301,7 @@ const Page = () => {
                   />
                 </div>
               </div>
-              <div className="col-sm-8 col-md-6" >
+              <div className="col-sm-8 col-md-6">
                 <div className="idea_working">
                   <h3>Flexibility in Execution</h3>
                   <p>
@@ -278,7 +314,7 @@ const Page = () => {
             </div>
           </div>
           <div className="work_section">
-            <div className="col-sm-4 col-md-6 pull-right" >
+            <div className="col-sm-4 col-md-6 pull-right">
               <div className="image_working">
                 <Image
                   unoptimized={true}
@@ -289,7 +325,7 @@ const Page = () => {
                 />
               </div>
             </div>
-            <div className="col-sm-8 col-md-6 pull-left" >
+            <div className="col-sm-8 col-md-6 pull-left">
               <div className="idea_working">
                 <h3>User-End Priority</h3>
                 <p>
