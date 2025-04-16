abaaf715abead64135ed8fb687c58f5528fe883d
Manish <manish@logicspice.com>
2025-04-16 18:48:00 +0530

    added changes

--- src/app/Components/home/TakeaLook.js
+++ src/app/Components/home/TakeaLook.js
@@ -5,12 +5,11 @@ const TakeaLook = () => {
   return (
     <section class="take-a-look">
       <div class="container">
-        <div class="headings">
+        <div class="headings ">
           {/* <h2>TAKE A LOOK AT WHAT WE HAVE BEEN WORKING ON</h2> */}
           <p
-            style={{
-              fontSize: "26px",
-            }}
+          className="MainHeading"
+            
           >
             Empowering Progress Through AI-Driven Technology, 
             Designed for the Future You Envision.
